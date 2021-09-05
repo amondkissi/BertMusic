fc7c474d2a5d2ed43f54b5f39887608a0553636a
Mac Owner <macbookpro@iMac.attlocal.net>
2021-09-05 13:24:46 -0500

    first commit

---
+++ store/index.php
@@ -0,0 +1,1 @@
+<meta http-equiv="refresh" content="0; URL='https://bert-music.creator-spring.com/'" />
---
+++ videos/index.php
@@ -0,0 +1,1 @@
+<meta http-equiv="refresh" content="0; URL='https://www.youtube.com/watch?v=qnMBJvfcRGY&list=PLyMjjL8d-xCEYuB4kNHkdkHQSKTza-9ob&index=2'" />
